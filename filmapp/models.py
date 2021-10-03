61803aa99c1fbe029fd4afbc7a34ff238565063d
Drenor777 <paweljakubczyk1@gmail.com>
2021-10-03 02:50:54 +0200

    new function in film + small change in formats

--- filmapp/models.py
+++ filmapp/models.py
@@ -8,7 +8,10 @@ class Film(models.Model):
     review = models.TextField(default='')
     premiere = models.DateField(null=True, blank=True)
     imbd_rating = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
-    poster = models.ImageField(upload_to='posters', null=True, blank=False)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+
+    def title_with_year(self):
+        return '{} ({})'.format(str(self.title), str(self.year))
 
     def __str__(self):
-        return str(self.title) + '(' + str(self.year) + ')'
+        return self.title_with_year()
