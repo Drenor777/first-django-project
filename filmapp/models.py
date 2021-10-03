b0291567ad94898cba16137b307d8c4481f642c8
Drenor777 <paweljakubczyk1@gmail.com>
2021-10-03 02:20:30 +0200

    creating model Film

--- filmapp/models.py
+++ filmapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=64, blank=False, unique=True)
+    prize = models.CharField(max_length=64, null=True)
+    year = models.PositiveSmallIntegerField(default=2000)
+
+    def __str__(self):
+        return str(self.title) + '(' + str(self.year) + ')'
