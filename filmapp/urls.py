b351197d3e8fcea316c0644ff9117b36e5fbcd42
Drenor777 <paweljakubczyk1@gmail.com>
2021-10-18 16:45:40 +0200

    CRUD end + movement

--- filmapp/urls.py
+++ filmapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from filmapp.views import homepage, new_film, edit_film, delete_film
 
 urlpatterns = [
-    path('', homepage),
-    path('new/', new_film),
-    path('edit/<int:id>/', edit_film),
-    path('delete/<int:id>/', delete_film),
+    path('', homepage, name="homepage"),
+    path('new/', new_film, name="new_film"),
+    path('edit/<int:id>/', edit_film, name='edit_film'),
+    path('delete/<int:id>/', delete_film, name="delete_film"),
 ]
